b99e13b89fcabb311fec3b1e779a4cdcf7cc0da6
Sara Vieira <hey@iamsaravieira.com>
2018-02-27 23:33:18 +0000

    add paid

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const typeDefs = `
     title: String,
     url: String
   }
-  type Link { url: String, type: [String], meta: Meta }
+  type Link { url: String, type: [String], meta: Meta, paid: Boolean }
 `
 
 const resolvers = {
